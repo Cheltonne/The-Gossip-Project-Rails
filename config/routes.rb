994da6fa90c0d92d1335a6e328cf48c2b060ea66
Cheltonne <chelton1506@gmail.com>
2020-02-17 22:36:38 +0100

    oui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root to: 'welcome#index'
   # get 'welcome', to: 'welcome#index'
   get 'contact', to: 'contact#show'
   get 'team', to: 'team#show'
